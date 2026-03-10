b89d5f88070e3daa46042c4f425d1d1ed60f2f03
raphaellith <raphaellith@gmail.com>
2026-03-10 23:12:14 +0000

    docs: NgramsFinder.py

--- NgramsFinder.py
+++ NgramsFinder.py
@@ -1,3 +1,9 @@
+"""
+Provides a helper class for retrieving collapsed relative match counts from the ngrams.dev API.
+
+See https://ngrams.dev/.
+"""
+
 import requests
 
 class NgramsFinder:
@@ -5,6 +11,12 @@ class NgramsFinder:
         self.request_session = requests.Session()
 
     def get_collapsed_relative_match_count(self, ngram: str) -> float:
+        """
+        Retrieves the collapsed relative match count for a given ngram.
+        The retrieval is case-insensitive and limited to English corpora.
+        :param ngram: The ngram for which to retrieve the collapsed relative match count.
+        :return: The collapsed relative match count for the given ngram.
+        """
         print("Getting collapsed relative match count for ngram: " + ngram)
 
         url = f"https://api.ngrams.dev/eng/search?query={ngram}&flags=cr"
