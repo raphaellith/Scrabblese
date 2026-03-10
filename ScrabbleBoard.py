8d5aef0343f344ac7f6cf180b44950383ea2b936
raphaellith <raphaellith@gmail.com>
2026-03-10 22:19:35 +0000

    added ScrabbleBoard; replaced 'x' and 'y' with 'row' and 'col'

---
+++ ScrabbleBoard.py
@@ -0,0 +1,16 @@
+class ScrabbleBoard:
+    def __init__(self, size):
+        self.size = size
+        self.__board = [["" for _ in range(self.size)] for __ in range(self.size)]
+
+    def __str__(self) -> str:
+        return "\n".join(["".join([s if s else "_" for s in row]) for row in self.board])
+
+    def add_word(self, str):
+        pass
+
+    def get_cell(self, col, row):
+        return self.__board[row][col]
+
+    def set_cell(self, col, row, value):
+        self.__board[row][col] = value
