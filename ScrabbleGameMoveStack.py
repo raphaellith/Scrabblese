41c559d24021004618a58bb302eeae69195e9420
raphaellith <raphaellith@gmail.com>
2026-03-10 23:05:52 +0000

    replaced Stack.py with ScrabbleGameMoveStack.py with stricter types

---
+++ ScrabbleGameMoveStack.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+from ScrabbleGameMove import ScrabbleGameMove
+
+
+class ScrabbleGameMoveStack:
+    def __init__(self):
+        self._stack: list[ScrabbleGameMove] = []
+
+    def push(self, move: ScrabbleGameMove):
+        self._stack.append(move)
+
+    def pop(self) -> ScrabbleGameMove:
+        return self._stack.pop()
+
+    def peek(self) -> ScrabbleGameMove:
+        return self._stack[-1]
+
+    def as_list(self) -> list[ScrabbleGameMove]:
+        return self._stack[:]
