797471dd186b38c13c976b080c14f1ef67809aba
raphaellith <raphaellith@gmail.com>
2026-03-12 13:24:08 +0000

    docstrings, renamed variables, comments: ScrabbleseAnalysisFuncs.py

--- ScrabbleseAnalysisFuncs.py
+++ ScrabbleseAnalysisFuncs.py
@@ -1,3 +1,7 @@
+"""
+Provides core functions for analysing words used in Scrabble games.
+"""
+
 from collections import Counter
 import csv
 
@@ -8,16 +12,42 @@ from NgramsFinder import NgramsFinder
 
 
 # CSV reader and writer settings
-CSV_DELIMITER = " "
-CSV_QUOTECHAR = "|"
+CSV_DELIMITER: str = " "
+CSV_QUOTECHAR: str = "|"
 
 
 def scrape_and_output_words_from_scrabble_games_to_text_file(text_file_path: str, max_files_to_read: int = -1):
+    """
+    Scrapes GCG files from Cross-Tables.com and outputs the words in each game to a text file.
+    The words are separated by newlines in the text file.
+    :param text_file_path: The path to the text file where the words will be written.
+    :param max_files_to_read: The maximum number of GCG files to read. If set to -1, all files will be read.
+    :return: None
+    """
+
     scraper = GcgScraper()
     scraper.output_words_to_file(text_file_path, max_files_to_read)
 
 
 def read_scrabble_words_from_text_file_and_compute_english_and_scrabble_probabilities_to_be_stored_in_csv_file(text_file_path: str, csv_file_path: str):
+    """
+    Reads the words from a text file, computes the English and Scrabble probabilities for each word,
+    and stores the results in a CSV file.
+    The CSV file is formatted as follows:
+
+    [WORD], [ENGLISH_PROBABILITY], [SCRABBLE_PROBABILITY]
+
+    where
+    - [WORD] is the word;
+    - [ENGLISH_PROBABILITY] is the English probability of the word; and
+    - [SCRABBLE_PROBABILITY] is the Scrabble probability of the word.
+
+    :param text_file_path: The path to the text file.
+    The contents of this file are assumed to be the words extracted from Scrabble games, as scraped via scrape_and_output_words_from_scrabble_games_to_text_file.
+    :param csv_file_path: The path to the CSV file where the probabilities will be stored.
+    :return: None
+    """
+
     scrabble_frequency_counter = Counter()
 
     with open(text_file_path) as text_file:
@@ -25,15 +55,18 @@ def read_scrabble_words_from_text_file_and_compute_english_and_scrabble_probabil
             word = line.removesuffix("\n")
             scrabble_frequency_counter[word] += 1
 
-    scrabble_frequency_total = sum(scrabble_frequency_counter.values())
+    # The number of words extracted from all Scrabble games
+    # Different instances of the same word are counted separately
+    num_of_scrabble_words = sum(scrabble_frequency_counter.values())
 
     ngrams_finder = NgramsFinder()
 
     with open(csv_file_path, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile, delimiter=CSV_DELIMITER, quotechar=CSV_QUOTECHAR, quoting=csv.QUOTE_MINIMAL)
         for word in scrabble_frequency_counter:
-            scrabble_probability = scrabble_frequency_counter[word] / scrabble_frequency_total
-            ngrams_probability = ngrams_finder.get_collapsed_relative_match_count(word)
+            scrabble_probability: float = scrabble_frequency_counter[word] / num_of_scrabble_words
+            ngrams_probability: float = ngrams_finder.get_collapsed_relative_match_count(word)
+
             csv_writer.writerow([
                 word,
                 scrabble_probability,
@@ -42,9 +75,19 @@ def read_scrabble_words_from_text_file_and_compute_english_and_scrabble_probabil
 
 
 def read_csv_file_and_display_plot(csv_file_path: str):
-    words = []
-    scrabble_probabilities = []
-    ngrams_probabilities = []
+    """
+    Reads the English and Scrabble probabilities of words listed in a CSV file
+    and displays a plot of the two probabilities (via Matplotlib) of each word.
+
+    The CSV file is assumed to have the format specified in
+    read_scrabble_words_from_text_file_and_compute_english_and_scrabble_probabilities_to_be_stored_in_csv_file.
+
+    :param csv_file_path: The path to the CSV file.
+    :return: None
+    """
+    words: list[str] = []
+    scrabble_probabilities: list[float] = []
+    ngrams_probabilities: list[float] = []
 
     with open(csv_file_path, newline='') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=CSV_DELIMITER, quotechar=CSV_QUOTECHAR)
