cde3fceffb2c53f7f98098e4f65e9afe23127c81
raphaellith <raphaellith@gmail.com>
2026-03-12 12:37:27 +0000

    docstrings and type hints: ScrabbleBoard.py

--- Scrabble/ScrabbleBoard.py
+++ Scrabble/ScrabbleBoard.py
@@ -12,17 +12,49 @@ class ScrabbleBoard:
     each ScrabbleGame may have multiple instances of ScrabbleBoard.
     """
     def __init__(self, size):
-        self.size = size
+        """
+        Initialises a ScrabbleBoard of a given size.
+
+        The attribute self.__board represents the board as a 2D list of strings. These strings are either:
+        - empty (""), indicating an empty cell; or
+        - a single letter, indicating a cell containing a tile.
+
+        :param size: The width and height of the board.
+        """
+        self.size: int = size
         self.__board: list[list[str]] = [["" for _ in range(self.size)] for __ in range(self.size)]
 
     def __str__(self) -> str:
+        """
+        Returns a string representation of the board, with each row separated by a newline character.
+        Empty cells are represented by an underscore.
+        :return: A string representation of the board.
+        """
         return "\n".join(["".join([s if s else "_" for s in row]) for row in self.__board])
 
     def get_cell(self, col, row) -> str:
+        """
+        Returns the value of the cell at the given (column, row) position.
+        :param col: The column position of the cell.
+        :param row: The row position of the cell.
+        :return: Either an empty string (""), indicating an empty cell;
+        or a single letter, indicating a cell containing a tile.
+        """
         return self.__board[row][col]
 
     def set_cell(self, col, row, value):
+        """
+        Sets the value of the cell at the given (column, row) position.
+        :param col: The column position of the cell.
+        :param row: The row position of the cell.
+        :param value: The value to set.
+        :return: None.
+        """
         self.__board[row][col] = value
 
     def deep_copy(self) -> 'ScrabbleBoard':
+        """
+        Returns a deep copy of the board.
+        :return: A deep copy of the board.
+        """
         return deepcopy(self)
