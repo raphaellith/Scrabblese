e92c8eda05645145e3bfc76170a808747a9c6789
raphaellith <raphaellith@gmail.com>
2026-04-06 22:42:10 +0800

    feat: cascaded deletes in ScrabbleGameWordEntity

--- models/scrabble_game_word_entity.py
+++ models/scrabble_game_word_entity.py
@@ -6,8 +6,9 @@ from models.scrabble_word_entity import ScrabbleWordEntity
 
 
 class ScrabbleGameWordEntity(BaseModel):
-    scrabble_game_id = ForeignKeyField(ScrabbleGameEntity, backref='words')
-    scrabble_word_id = ForeignKeyField(ScrabbleWordEntity, backref='games')
+    # When a ScrabbleGameEntity or ScrabbleWordEntity is deleted, all related ScrabbleGameWordEntities are also deleted
+    scrabble_game_id = ForeignKeyField(ScrabbleGameEntity, backref='words', on_delete='CASCADE')
+    scrabble_word_id = ForeignKeyField(ScrabbleWordEntity, backref='games', on_delete='CASCADE')
     count = IntegerField(default=0)
 
     class Meta:
