657aeebb57b6c7cc0942a075ac64839832eaedc0
raphaellith <raphaellith@gmail.com>
2026-04-06 22:43:41 +0800

    comments: clarified scope of cascades

--- models/scrabble_game_word_entity.py
+++ models/scrabble_game_word_entity.py
@@ -6,7 +6,10 @@ from models.scrabble_word_entity import ScrabbleWordEntity
 
 
 class ScrabbleGameWordEntity(BaseModel):
+    # Cascades:
     # When a ScrabbleGameEntity or ScrabbleWordEntity is deleted, all related ScrabbleGameWordEntities are also deleted
+    # Note however that orphaned ScrabbleWordEntities (those unreferenced by any game) are not automatically deleted
+
     scrabble_game_id = ForeignKeyField(ScrabbleGameEntity, backref='words', on_delete='CASCADE')
     scrabble_word_id = ForeignKeyField(ScrabbleWordEntity, backref='games', on_delete='CASCADE')
     count = IntegerField(default=0)
