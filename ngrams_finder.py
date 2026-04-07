cb196bfdd655d1a0cb90332e3d695428567bf350
raphaellith <raphaellith@gmail.com>
2026-04-07 14:31:03 +0800

    refactor: typing in ngrams_finder.py

--- ngrams_finder.py
+++ ngrams_finder.py
@@ -7,8 +7,8 @@ See https://ngrams.dev/.
 import requests
 
 class NgramsFinder:
-    def __init__(self):
-        self.request_session = requests.Session()
+    def __init__(self) -> None:
+        self.request_session: requests.Session = requests.Session()
 
     def get_collapsed_relative_match_count(self, ngram: str) -> float:
         """
@@ -19,7 +19,7 @@ class NgramsFinder:
         """
         print("Getting collapsed relative match count for ngram: " + ngram)
 
-        url = f"https://api.ngrams.dev/eng/search?query={ngram}&flags=cr"
+        url: str = f"https://api.ngrams.dev/eng/search?query={ngram}&flags=cr"
         response: requests.Response = self.request_session.get(url)
         response_dict: dict = response.json()
         ngram_results: list = response_dict["ngrams"]
