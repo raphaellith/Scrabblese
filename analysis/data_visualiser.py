10ee953f72221f5ac5e2858e129a01cd3271eaa4
raphaellith <raphaellith@gmail.com>
2026-04-07 13:18:54 +0800

    feat: converted data_visualiser.py to a class with where expression support

--- analysis/data_visualiser.py
+++ analysis/data_visualiser.py
@@ -3,107 +3,102 @@ from models.scrabble_game_word_entity import ScrabbleGameWordEntity
 from models.scrabble_word_entity import ScrabbleWordEntity
 from models.scrabble_game_entity import ScrabbleGameEntity
 
-from peewee import fn
+from peewee import fn, Expression
 from matplotlib import pyplot as plt
 
 
-def get_words_and_probabilities(include_listed_games: bool = True, include_unlisted_games: bool = True):
-    initialise_database()
+class ScrabbleseDataVisualiser:
+    def __init__(self):
+        self._where_expressions: list[Expression] = []
 
-    # Handle case where both are False: return empty
-    if not include_listed_games and not include_unlisted_games:
-        return [], [], []
+    def add_where_expression(self, where_expression: Expression):
+        self._where_expressions.append(where_expression)
 
-    total_number_of_scrabble_plays = get_total_number_of_scrabble_plays(
-        include_listed_games=include_listed_games,
-        include_unlisted_games=include_unlisted_games
-    )
+    def get_query_for_retrieving_words_and_probabilities(self):
+        total_number_of_scrabble_plays = self.get_total_number_of_scrabble_plays()
 
-    if total_number_of_scrabble_plays == 0:
-        return [], [], []
+        if total_number_of_scrabble_plays == 0:
+            raise ZeroDivisionError("No scrabble plays found, so probabilities cannot be computed.")
 
-    query = (
-        ScrabbleWordEntity
-        .select(
-            ScrabbleWordEntity.text,
-            ScrabbleWordEntity.ngrams_probability,
-            (fn.SUM(ScrabbleGameWordEntity.count) / float(total_number_of_scrabble_plays)).alias("scrabble_probability")
+        query = (
+            ScrabbleWordEntity
+            .select(
+                ScrabbleWordEntity.text,
+                ScrabbleWordEntity.ngrams_probability,
+                (fn.SUM(ScrabbleGameWordEntity.count) / float(total_number_of_scrabble_plays)).alias(
+                    "scrabble_probability")
+            )
+            .join(ScrabbleGameWordEntity, on=(ScrabbleGameWordEntity.scrabble_word_id == ScrabbleWordEntity.id))
+            .join(ScrabbleGameEntity, on=(ScrabbleGameWordEntity.scrabble_game_id == ScrabbleGameEntity.id))
         )
-        .join(ScrabbleGameWordEntity, on=(ScrabbleGameWordEntity.scrabble_word_id == ScrabbleWordEntity.id))
-        .join(ScrabbleGameEntity, on=(ScrabbleGameWordEntity.scrabble_game_id == ScrabbleGameEntity.id))
-    )
 
-    # If both are true, no filter is needed
-    if include_listed_games and not include_unlisted_games:
-        query = query.where(ScrabbleGameEntity.is_on_list_page == True)
-    elif not include_listed_games and include_unlisted_games:
-        query = query.where(ScrabbleGameEntity.is_on_list_page == False)
+        for where_expression in self._where_expressions:
+            query = query.where(where_expression)
 
-    query = query.group_by(ScrabbleWordEntity.text, ScrabbleWordEntity.ngrams_probability)
-    query = query.order_by(ScrabbleWordEntity.text)
+        query = query.group_by(ScrabbleWordEntity.text, ScrabbleWordEntity.ngrams_probability)
+        query = query.order_by(ScrabbleWordEntity.text)
 
-    words = []
-    ngrams_probabilities = []
-    scrabble_probabilities = []
+        return query
 
-    for row in query:
-        words.append(row.text)
-        ngrams_probabilities.append(row.ngrams_probability)
-        scrabble_probabilities.append(row.scrabble_probability)
+    def get_words_and_probabilities(self):
+        initialise_database()
 
-    return words, ngrams_probabilities, scrabble_probabilities
+        query = self.get_query_for_retrieving_words_and_probabilities()
 
+        words = []
+        ngrams_probabilities = []
+        scrabble_probabilities = []
 
-def get_total_number_of_scrabble_plays(include_listed_games: bool = True, include_unlisted_games: bool = True):
-    initialise_database()
+        for row in query:
+            words.append(row.text)
+            ngrams_probabilities.append(row.ngrams_probability)
+            scrabble_probabilities.append(row.scrabble_probability)
 
-    if not include_listed_games and not include_unlisted_games:
-        return 0
+        return words, ngrams_probabilities, scrabble_probabilities
 
-    query = (ScrabbleGameWordEntity
-             .select(fn.SUM(ScrabbleGameWordEntity.count))
-             .join(ScrabbleGameEntity, on=(ScrabbleGameWordEntity.scrabble_game_id == ScrabbleGameEntity.id))
-    )
+    def get_total_number_of_scrabble_plays(self):
+        initialise_database()
 
-    # If both are true, no filter is needed
-    if include_listed_games and not include_unlisted_games:
-        query = query.where(ScrabbleGameEntity.is_on_list_page == True)
-    elif not include_listed_games and include_unlisted_games:
-        query = query.where(ScrabbleGameEntity.is_on_list_page == False)
+        query = (
+            ScrabbleGameWordEntity
+            .select(fn.SUM(ScrabbleGameWordEntity.count))
+            .join(ScrabbleGameEntity, on=(ScrabbleGameWordEntity.scrabble_game_id == ScrabbleGameEntity.id))
+            .join(ScrabbleWordEntity, on=(ScrabbleGameWordEntity.scrabble_word_id == ScrabbleWordEntity.id))
+        )
 
-    return query.scalar() or 0
+        for where_expression in self._where_expressions:
+            query = query.where(where_expression)
 
+        return query.scalar() or 0
 
-def show_plot(words: list[str], ngrams_probabilities: list[float],
-              scrabble_probabilities: list[float], x_axis_label_for_ngrams_probabilities: str = "",
+    def show_plot(self, x_axis_label_for_ngrams_probabilities: str = "",
               y_axis_label_for_scrabble_probabilities: str = "", logarithmic: bool = False,
               annotated: bool = True):
-    """
-    Displays a plot (via Matplotlib) of the ngrams and Scrabble probabilities of each word.
+        """
+        Displays a plot (via Matplotlib) of the ngrams and Scrabble probabilities of each word.
 
-    :param words: A list of words.
-    :param ngrams_probabilities: The ngrams probabilities of the listed words.
-    :param scrabble_probabilities: The scrabble probabilities of the listed words.
-    :param x_axis_label_for_ngrams_probabilities: The label for the x-axis of the plot.
-    :param y_axis_label_for_scrabble_probabilities: The label for the y-axis of the plot.
-    :param logarithmic: Whether to use a logarithmic scale for the axes of the plot.
-    :param annotated: Whether to annotate each data point with the word it represents.
-    :return: None
-    """
-    plt.scatter(ngrams_probabilities, scrabble_probabilities, marker=".")
+        :param x_axis_label_for_ngrams_probabilities: The label for the x-axis of the plot.
+        :param y_axis_label_for_scrabble_probabilities: The label for the y-axis of the plot.
+        :param logarithmic: Whether to use a logarithmic scale for the axes of the plot.
+        :param annotated: Whether to annotate each data point with the word it represents.
+        :return: None
+        """
+        words, ngrams_probabilities, scrabble_probabilities = self.get_words_and_probabilities()
 
-    scale_option = "log" if logarithmic else "linear"
-    plt.xscale(scale_option)
-    plt.yscale(scale_option)
+        plt.scatter(ngrams_probabilities, scrabble_probabilities, marker=".")
 
-    if annotated:
-        for i, word in enumerate(words):
-            plt.annotate(word, (ngrams_probabilities[i], scrabble_probabilities[i]))
+        scale_option = "log" if logarithmic else "linear"
+        plt.xscale(scale_option)
+        plt.yscale(scale_option)
 
-    if x_axis_label_for_ngrams_probabilities:
-        plt.xlabel(x_axis_label_for_ngrams_probabilities)
+        if annotated:
+            for i, word in enumerate(words):
+                plt.annotate(word, (ngrams_probabilities[i], scrabble_probabilities[i]))
 
-    if y_axis_label_for_scrabble_probabilities:
-        plt.ylabel(y_axis_label_for_scrabble_probabilities)
+        if x_axis_label_for_ngrams_probabilities:
+            plt.xlabel(x_axis_label_for_ngrams_probabilities)
 
-    plt.show()
+        if y_axis_label_for_scrabble_probabilities:
+            plt.ylabel(y_axis_label_for_scrabble_probabilities)
+
+        plt.show()
