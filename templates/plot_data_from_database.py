86548a3f46bfcf3643cc85b277f86f7e1f17dcb5
raphaellith <raphaellith@gmail.com>
2026-04-07 23:23:05 +0800

    feat: renamed data_plotter.py; removed commented code; added sample program

--- templates/plot_data_from_database.py
+++ templates/plot_data_from_database.py
@@ -6,24 +6,17 @@ from matplotlib import pyplot as plt
 from datahandling.database_reader import get_data_points
 
 
-data_points = get_data_points(select_listed_games=True, select_unlisted_games=True)
+data_points = get_data_points(select_listed_games=True, select_unlisted_games=False)
 
-
-# PREPROCESS DATA POINTS HERE
-
+data_points = [d for d in data_points if len(d.word) == 2]
 
 plt.scatter([d.ngrams_probability for d in data_points], [d.scrabble_play_count for d in data_points], marker=".")
 
+plt.xscale("log")
+plt.yscale("log")
 
-# SCALING
-# plt.xscale("log")
-# plt.yscale("log")
-
-
-# LABELLING DATA POINTS
-# for d in data_points:
-#     plt.annotate(d.word, (d.ngrams_probability, d.scrabble_play_count))
-
+for d in data_points:
+    plt.annotate(d.word, (d.ngrams_probability, d.scrabble_play_count))
 
 plt.xlabel("Ngrams probability")
 plt.ylabel("Scrabble play count")
