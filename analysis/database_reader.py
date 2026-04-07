f49431686067169f1d39e399004e3e43ff8a0998
raphaellith <raphaellith@gmail.com>
2026-04-07 14:04:30 +0800

    refactor: database_reader.py as collection of functions rather than class (as all methods are "static")

--- analysis/database_reader.py
+++ analysis/database_reader.py
@@ -8,89 +8,88 @@ from peewee import fn
 from matplotlib import pyplot as plt
 
 
-class ScrabbleseDatabaseReader:
-    def __init__(self):
-        pass
+def get_total_number_of_scrabble_plays():
+    initialise_database()
 
-    def get_total_number_of_scrabble_plays(self):
-        initialise_database()
+    query = (
+        ScrabbleGameWordEntity
+        .select(fn.SUM(ScrabbleGameWordEntity.count))
+        .join(ScrabbleGameEntity, on=(ScrabbleGameWordEntity.scrabble_game_id == ScrabbleGameEntity.id))
+        .join(ScrabbleWordEntity, on=(ScrabbleGameWordEntity.scrabble_word_id == ScrabbleWordEntity.id))
+    )
 
-        query = (
-            ScrabbleGameWordEntity
-            .select(fn.SUM(ScrabbleGameWordEntity.count))
-            .join(ScrabbleGameEntity, on=(ScrabbleGameWordEntity.scrabble_game_id == ScrabbleGameEntity.id))
-            .join(ScrabbleWordEntity, on=(ScrabbleGameWordEntity.scrabble_word_id == ScrabbleWordEntity.id))
+    return query.scalar() or 0
+
+
+def get_query_for_retrieving_words_and_probabilities():
+    total_number_of_scrabble_plays = get_total_number_of_scrabble_plays()
+
+    if total_number_of_scrabble_plays == 0:
+        raise ZeroDivisionError("No scrabble plays found, so probabilities cannot be computed.")
+
+    query = (
+        ScrabbleWordEntity
+        .select(
+            ScrabbleWordEntity.text,
+            ScrabbleWordEntity.ngrams_probability,
+            (fn.SUM(ScrabbleGameWordEntity.count) / float(total_number_of_scrabble_plays)).alias(
+                "scrabble_probability")
         )
+        .join(ScrabbleGameWordEntity, on=(ScrabbleGameWordEntity.scrabble_word_id == ScrabbleWordEntity.id))
+        .join(ScrabbleGameEntity, on=(ScrabbleGameWordEntity.scrabble_game_id == ScrabbleGameEntity.id))
+    )
 
-        return query.scalar() or 0
+    query = query.group_by(ScrabbleWordEntity.text, ScrabbleWordEntity.ngrams_probability)
+    query = query.order_by(ScrabbleWordEntity.text)
 
-    def get_query_for_retrieving_words_and_probabilities(self):
-        total_number_of_scrabble_plays = self.get_total_number_of_scrabble_plays()
+    return query
 
-        if total_number_of_scrabble_plays == 0:
-            raise ZeroDivisionError("No scrabble plays found, so probabilities cannot be computed.")
 
-        query = (
-            ScrabbleWordEntity
-            .select(
-                ScrabbleWordEntity.text,
-                ScrabbleWordEntity.ngrams_probability,
-                (fn.SUM(ScrabbleGameWordEntity.count) / float(total_number_of_scrabble_plays)).alias(
-                    "scrabble_probability")
-            )
-            .join(ScrabbleGameWordEntity, on=(ScrabbleGameWordEntity.scrabble_word_id == ScrabbleWordEntity.id))
-            .join(ScrabbleGameEntity, on=(ScrabbleGameWordEntity.scrabble_game_id == ScrabbleGameEntity.id))
+def get_data_points():
+    initialise_database()
+
+    query = get_query_for_retrieving_words_and_probabilities()
+
+    data_points = [
+        ScrabbleseDataPoint(
+            word=row.text,
+            ngrams_probability=row.ngrams_probability,
+            scrabble_probability=row.scrabble_probability
         )
+        for row in query
+    ]
 
-        query = query.group_by(ScrabbleWordEntity.text, ScrabbleWordEntity.ngrams_probability)
-        query = query.order_by(ScrabbleWordEntity.text)
+    return data_points
 
-        return query
 
-    def get_data_points(self):
-        initialise_database()
-
-        query = self.get_query_for_retrieving_words_and_probabilities()
-
-        data_points = [
-            ScrabbleseDataPoint(
-                word=row.text,
-                ngrams_probability=row.ngrams_probability,
-                scrabble_probability=row.scrabble_probability
-            )
-            for row in query
-        ]
-
-        return data_points
-
-    # TODO: Extract this method to a separate class to the main program
-    def show_plot(self, x_axis_label_for_ngrams_probabilities: str = "",
+# TODO: Extract this method to a separate class or file
+def show_plot(x_axis_label_for_ngrams_probabilities: str = "",
               y_axis_label_for_scrabble_probabilities: str = "", logarithmic: bool = False,
               annotated: bool = True):
-        """
-        Displays a plot (via Matplotlib) of the ngrams and Scrabble probabilities of each word.
+    """
+    Displays a plot (via Matplotlib) of the ngrams and Scrabble probabilities of each word.
 
-        :param x_axis_label_for_ngrams_probabilities: The label for the x-axis of the plot.
-        :param y_axis_label_for_scrabble_probabilities: The label for the y-axis of the plot.
-        :param logarithmic: Whether to use a logarithmic scale for the axes of the plot.
-        :param annotated: Whether to annotate each data point with the word it represents.
-        :return: None
-        """
-        data_points = self.get_data_points()
-        plt.scatter([d.ngrams_probability for d in data_points], [d.scrabble_probability for d in data_points], marker=".")
+    :param x_axis_label_for_ngrams_probabilities: The label for the x-axis of the plot.
+    :param y_axis_label_for_scrabble_probabilities: The label for the y-axis of the plot.
+    :param logarithmic: Whether to use a logarithmic scale for the axes of the plot.
+    :param annotated: Whether to annotate each data point with the word it represents.
+    :return: None
+    """
+    data_points = get_data_points()
+    plt.scatter([d.ngrams_probability for d in data_points], [d.scrabble_probability for d in data_points], marker=".")
 
-        scale_option = "log" if logarithmic else "linear"
-        plt.xscale(scale_option)
-        plt.yscale(scale_option)
+    scale_option = "log" if logarithmic else "linear"
+    plt.xscale(scale_option)
+    plt.yscale(scale_option)
 
-        if annotated:
-            for d in data_points:
-                plt.annotate(d.word, (d.ngrams_probability, d.scrabble_probability))
+    if annotated:
+        for d in data_points:
+            plt.annotate(d.word, (d.ngrams_probability, d.scrabble_probability))
 
-        if x_axis_label_for_ngrams_probabilities:
-            plt.xlabel(x_axis_label_for_ngrams_probabilities)
+    if x_axis_label_for_ngrams_probabilities:
+        plt.xlabel(x_axis_label_for_ngrams_probabilities)
 
-        if y_axis_label_for_scrabble_probabilities:
-            plt.ylabel(y_axis_label_for_scrabble_probabilities)
+    if y_axis_label_for_scrabble_probabilities:
+        plt.ylabel(y_axis_label_for_scrabble_probabilities)
 
-        plt.show()
+    plt.show()
